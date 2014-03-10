010245666351e8dd7a898a2b3650e657fd5b0331
João Moura <jccmoura@gmail.com>
2014-03-10 18:55:18 +0000

    last

--- servidor.js
+++ servidor.js
@@ -1,12 +1,23 @@
 var shortens = [{}];
-var count = 0; 
+var count = 0;
+var acess = { 'mauricio@isban.pt' :  '1234', 'alfredo@isban.pt' : '1234'};
+
+var MAX_SESSION_SECONDS = 60 * 60 * 2;
+
+var HMAC_KEY = 'A';
+var HMAC_ALG = 'sha1';
+//var HMAC_KEY = process.env.KEY;
+
 
 var fs = require('fs');
 
+var assert = require('assert');
 var cors = require('cors');
 var express = require('express');
 var crypto = require('crypto');
 var server = express();
+
+assert(HMAC_KEY, 'KEY is not defined!');
 
 var serv = require('http').createServer(server)
 var io = require('socket.io').listen(serv);
@@ -18,6 +29,8 @@ server.use(express.json());
 server.use(express.urlencoded());
 server.use(express.static( __dirname + 'public'));
 server.use(cors());
+
+
 
 //*******************************************************************************
 //                                        Lista Shortens
@@ -45,13 +58,31 @@ function lista(req, res) {
 server.post('/', cria);
 
 function cria(req, res) {
+    
+    var token = req.header('X-Auth-Token');
+    
+   
+    if (! token ) {
+        console.log('Erro autenticação 1')
+      return res.send(401, 'bad auth');
+    }
+    var val = validToken(token);
+    console.log('Resultado validate: ', val, validate.toString());
+    
+    if (!val) {
+      console.log('Erro autenticação 2')
+      return res.send(401, 'bad auth');
+    }
+
+    
     console.log('Cria Shorten');
     var s = fs.ReadStream('public/shortens.txt');                                       //lê DB
     fs.readFile('public/shortens.txt', function (err, data) {
         if (err) throw err;
         shortens = JSON.parse(data);
         
-        var url = req.body.url;
+        var dirtyUrl = req.body.url;
+        var url = dirtyUrl.replace(/(<([^>]+)>)/ig, '');
         var hash = crypto.createHash('sha1').update(url).digest('base64').replace(/[=+/]/g, '').slice(0, 6);
         console.log('Valor URL : ', url);
         
@@ -74,6 +105,31 @@ function cria(req, res) {
         res.send(shortens[hash].id);    
         });
 }
+
+
+function validToken(token) {
+  console.log('Entra validate');
+  var obj;
+  try {
+    obj = JSON.parse(token);
+  } catch(ex) {
+    return false;
+  }
+
+  if (Math.floor(Date.now()/1000) - obj.ts > MAX_SESSION_SECONDS)
+    return false;
+
+  return obj.sign === generateSign(obj.email, obj.ts);
+}
+
+function generateSign(email, ts) {
+  var sign = crypto.createHmac(HMAC_ALG, HMAC_KEY).update(email+':'+ts).digest('base64');
+  return sign;
+}
+
+
+
+
 //*******************************************************************************
 //                                        Redirect
 //*******************************************************************************
@@ -117,6 +173,42 @@ io.sockets.on('connection', function (socket) {
     console.log('Conectado socket ');
 });
 
+//**********************************************************************************
+//                                                  Log In
+//**********************************************************************************
+server.post('/login', function(req, res) {
+  
+  console.log('Pedido autenticaçao', req.body);
+  var email = req.body.email;
+  var password = req.body.password;
+  console.log('Pedido autenticaçao', email, password);
 
+  if (!email || !password)
+    return res.send(400, 'bad request');
+
+  var valid = validate(email, password);
+
+  if (! valid)
+    return res.send(401, 'bad auth');
+
+  res.send(generateToken(email));
+});
+
+function validate(email, password) {
+  if (!acess[email]) 
+    return false;
+  else
+    return acess[email] === password;
+}
+
+function generateToken(email) {
+  var now = Math.floor(Date.now() / 1000);
+
+  var sign = crypto.createHmac(HMAC_ALG, HMAC_KEY).update(email+':'+now).digest('base64');
+
+  return JSON.stringify({ ts: now, email: email, sign: sign });
+}
+
+//**************************************************************************
 
 serv.listen(8000);
